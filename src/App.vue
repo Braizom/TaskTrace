c601c4f2ef9b4a9de9756e502206ea40bfe8d8b1
Braizom <a.a.r.boisseaux@student.vu.nl>
2025-02-27 18:45:01 -0500

    Added first header prototype

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,14 @@
 <template>
-  <nav>
-  </nav>
+  <Header/>
   <router-view/>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import MainHeader from '@/components/MainHeader.vue'
 
-nav {
-  padding: 30px;
+export default {
+  components: {
+    Header: MainHeader
+  }
 }
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+</script>
