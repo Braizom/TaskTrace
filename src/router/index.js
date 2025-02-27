48565c580be8debf066f0a888e4cee03fb50d33a
Braizom <a.a.r.boisseaux@student.vu.nl>
2025-02-27 17:58:05 -0500

    Added routing for new pages

--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,41 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import Home from '../views/Home.vue'
+import Login from '../views/Login.vue'
+import Parameters from '../views/Parameters.vue'
+import Profile from '../views/Profile.vue'
+import Register from '../views/Register.vue'
+import ToDolist from '../views/ToDoList.vue'
 
 const routes = [
   {
     path: '/',
     name: 'home',
-    component: HomeView
+    component: Home
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    path: '/login',
+    name: 'login',
+    component: Login
+  },
+  {
+    path: '/parameters',
+    name: 'parameters',
+    component: Parameters
+  },
+  {
+    path: '/profile',
+    name: 'profile',
+    component: Profile
+  },
+  {
+    path: '/register',
+    name: 'register',
+    component: Register
+  },
+  {
+    path: '/todolist',
+    name: 'todolist',
+    component: ToDolist
   }
 ]
 
