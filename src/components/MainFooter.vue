6eea4c12f263c947ffd73f5d659bbeaccc0bd01e
Braizom <a.a.r.boisseaux@student.vu.nl>
2025-02-28 00:14:53 -0500

    Styled header and footer

--- src/components/MainFooter.vue
+++ src/components/MainFooter.vue
@@ -2,7 +2,7 @@
   <footer class="fixed bottom-0 left-0 z-20 w-full p-4 bg-white border-t border-gray-200 shadow-sm ">
     <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
       <div class="flex items-center space-x-3 rtl:space-x-reverse">
-          <span class="text-sm text-gray-500 sm:text-center">© 2025 TaskTrack™. All Rights Reserved.
+          <span class="text-sm text-ttHeaderText sm:text-center">© 2025 TaskTrack™. All Rights Reserved.
         </span>
       </div>
       <button data-collapse-toggle="footer-nav" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200" aria-controls="footer-nav" aria-expanded="false">
@@ -12,7 +12,7 @@
         </svg>
       </button>
       <div class="hidden w-full md:block md:w-auto" id="footer-nav">
-        <ul class="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 sm:mt-0">
+        <ul class="flex flex-wrap items-center mt-3 text-sm font-medium text-ttHeaderText sm:mt-0">
           <li>
             <p class="me-4 md:me-6">About</p>
           </li>
--- src/components/MainHeader.vue
+++ src/components/MainHeader.vue
@@ -1,9 +1,9 @@
 <template>
-  <nav class="bg-white border-gray-200">
-    <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+  <nav class="bg-ttHeaderBg border-gray-200">
+    <div class=" max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
       <div class="flex items-center space-x-3 rtl:space-x-reverse">
         <router-link to="/">
-          <span class="self-center text-2xl font-semibold whitespace-nowrap">TaskTrack</span>
+          <span class="text-ttHeaderText self-center text-5xl font-semibold whitespace-nowrap">TaskTrack</span>
         </router-link>
       </div>
       <button data-collapse-toggle="navbar-default" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200" aria-controls="navbar-default" aria-expanded="false">
@@ -13,18 +13,18 @@
         </svg>
       </button>
       <div class="hidden w-full md:block md:w-auto" id="navbar-default">
-        <ul class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white">
+        <ul class="font-medium flex flex-col p-4 md:p-0 mt-4 border rounded-lg md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0">
           <li>
-            <router-link to="/" class="block py-2 px-3 text-white bg-blue-700 rounded-sm md:bg-transparent md:text-blue-700 md:p-0" aria-current="page">Home</router-link>
+            <router-link to="/" class="bg-ttHeaderBtn py-1 px-1 text-ttHeaderText rounded-md ">Home</router-link>
           </li>
           <li>
-            <router-link to="/profile" class="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0">Profile</router-link>
+            <router-link to="/profile" class="bg-ttHeaderBtn block py-1 px-1 text-ttHeaderText rounded-md md:border-0">Profile</router-link>
           </li>
           <li>
-            <router-link to="/parameters" class="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0">Parameters</router-link>
+            <router-link to="/parameters" class="bg-ttHeaderBtn block py-1 px-1 text-ttHeaderText rounded-md md:border-0">Parameters</router-link>
           </li>
           <li>
-            <router-link to="/login" class="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0">Login</router-link>
+            <router-link to="/login" class="bg-ttHeaderBtn block py-1 px-1 text-ttHeaderText rounded-md md:border-0">Login</router-link>
           </li>
         </ul>
       </div>
