81460b855eb43363ab02b6f120195191aae7e598
Braizom <a.a.r.boisseaux@student.vu.nl>
2025-02-28 23:43:54 -0500

    Split Home page contents in components

---
+++ src/components/HomeThemes.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="m-5">
+    <h1 class="text-3xl font-semibold">Your Themes</h1>
+    <div class="grid grid-cols-2 md:grid-cols-3 gap-5 p-10">
+      <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow-sm">
+        <a href="#">
+          <img class="rounded-t-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-1.jpg" alt="" />
+        </a>
+        <div class="p-5">
+          <a href="#">
+            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">Books</h5>
+          </a>
+          <p class="mb-3 font-normal text-gray-700">You have 3 “Books” To-Do List</p>
+        </div>
+      </div>
+      <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow-sm">
+        <a href="#">
+          <img class="rounded-t-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-1.jpg" alt="" />
+        </a>
+        <div class="p-5">
+          <a href="#">
+            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">Video Games</h5>
+          </a>
+          <p class="mb-3 font-normal text-gray-700">You have 2 “Video Games” To-Do List</p>
+        </div>
+      </div>
+      <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow-sm">
+        <a href="#">
+          <img class="rounded-t-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-1.jpg" alt="" />
+        </a>
+        <div class="p-5">
+          <a href="#">
+            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">Musics</h5>
+          </a>
+          <p class="mb-3 font-normal text-gray-700">You have 1 “Musics” To-Do List</p>
+        </div>
+      </div>
+      <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow-sm">
+        <a href="#">
+          <img class="rounded-t-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-1.jpg" alt="" />
+        </a>
+        <div class="p-5">
+          <a href="#">
+            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">Movies</h5>
+          </a>
+          <p class="mb-3 font-normal text-gray-700">You have 1 “Movies” To-Do List</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
