4b61251f38af2b6e9f1aa2b02bdad471c31e5e55
Braizom <a.a.r.boisseaux@student.vu.nl>
2025-03-02 15:07:04 -0500

    Added To Do List elements

---
+++ src/components/ToDoListElem.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="w-full md:block md:w-auto mx-20">
+    <ToDoListElemMenu/>
+    <ul>
+      <li>
+        <div class="p-6 bg-white border border-gray-200 rounded-lg shadow-sm">
+          <h3 class="mb-5 text-lg font-medium text-gray-900">Oliver Twist, Charles Dickens</h3>
+          <ul class="grid w-full gap-6 md:grid-cols-3">
+            <li>
+              <input type="radio" id="1" name="hosting" value="1" class="hidden peer">
+              <label for="1" class="text-lg font-semibold inline-flex items-center justify-between w-full p-5 text-gray-500 bg-white border border-gray-200 rounded-lg cursor-pointer  peer-checked:bg-green-600  peer-checked:text-white hover:text-gray-600 hover:bg-gray-100 ">
+                To Do
+              </label>
+            </li>
+            <li>
+              <input type="radio" id="2" name="hosting" value="2" class="hidden peer">
+              <label for="2" class="text-lg font-semibold inline-flex items-center justify-between w-full p-5 text-gray-500 bg-white border border-gray-200 rounded-lg cursor-pointer  peer-checked:bg-green-600  peer-checked:text-white hover:text-gray-600 hover:bg-gray-100 ">
+                Doing
+              </label>
+            </li>
+            <li>
+              <input type="radio" id="3" name="hosting" value="3" class="hidden peer">
+              <label for="3" class="text-lg font-semibold inline-flex items-center justify-between w-full p-5 text-gray-500 bg-white border border-gray-200 rounded-lg cursor-pointer  peer-checked:bg-green-600  peer-checked:text-white hover:text-gray-600 hover:bg-gray-100 ">
+                Done
+              </label>
+            </li>
+          </ul>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import ToDoListElemMenu from '@/components/ToDoListElemMenu.vue'
+
+export default {
+  components: {
+    ToDoListElemMenu
+  }
+}
+</script>
--- src/views/ToDoListView.vue
+++ src/views/ToDoListView.vue
@@ -1,64 +1,33 @@
 <template>
-  <div class="mx-40">
+  <div class="mx-44">
     <ToDoListMain/>
-    <div id="accordion-collapse" data-accordion="collapse" class="mb-10 mt-10">
-      <h2 id="accordion-collapse-heading-1">
-        <button type="button" class="flex items-center justify-between w-full p-5 font-medium rtl:text-right text-gray-500 border border-b-0 border-gray-200 rounded-t-xl focus:ring-4 focus:ring-gray-200 hover:bg-gray-100" data-accordion-target="#accordion-collapse-body-1" aria-expanded="true" aria-controls="accordion-collapse-body-1">
-          <span>English Books</span>
-          <svg data-accordion-icon class="w-3 h-3 rotate-180 shrink-0" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
-            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5 5 1 1 5"/>
-          </svg>
-        </button>
-      </h2>
-      <div id="accordion-collapse-body-1" class="hidden" aria-labelledby="accordion-collapse-heading-1">
-        <div class="p-5 border border-b-0 border-gray-200">
-          <p class="mb-2 text-gray-500">Flowbite is an open-source library of interactive components built on top of Tailwind CSS including buttons, dropdowns, modals, navbars, and more.</p>
-          <p class="text-gray-500">Check out this guide to learn how to <a href="/docs/getting-started/introduction/" class="text-blue-600 ">get started</a> and start developing websites even faster with components on top of Tailwind CSS.</p>
-        </div>
-      </div>
-      <h2 id="accordion-collapse-heading-2">
-        <button type="button" class="flex items-center justify-between w-full p-5 font-medium rtl:text-right text-gray-500 border border-b-0 border-gray-200 focus:ring-4 focus:ring-gray-200  hover:bg-gray-100 " data-accordion-target="#accordion-collapse-body-2" aria-expanded="false" aria-controls="accordion-collapse-body-2">
-          <span>French Books</span>
-          <svg data-accordion-icon class="w-3 h-3 rotate-180 shrink-0" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
-            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5 5 1 1 5"/>
-          </svg>
-        </button>
-      </h2>
-      <div id="accordion-collapse-body-2" class="hidden" aria-labelledby="accordion-collapse-heading-2">
-        <div class="p-5 border border-b-0 border-gray-200">
-          <p class="mb-2 text-gray-500 ">Flowbite is first conceptualized and designed using the Figma software so everything you see in the library has a design equivalent in our Figma file.</p>
-          <p class="text-gray-500 ">Check out the <a href="https://flowbite.com/figma/" class="text-blue-600 ">Figma design system</a> based on the utility classes from Tailwind CSS and components from Flowbite.</p>
-        </div>
-      </div>
-      <h2 id="accordion-collapse-heading-3">
-        <button type="button" class="flex items-center justify-between w-full p-5 font-medium rtl:text-right text-gray-500 border border-gray-200 rounded-b-xl focus:ring-4 focus:ring-gray-200  hover:bg-gray-100 " data-accordion-target="#accordion-collapse-body-3" aria-expanded="false" aria-controls="accordion-collapse-body-3">
-          <span>Science Books</span>
-          <svg data-accordion-icon class="w-3 h-3 rotate-180 shrink-0" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
-            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5 5 1 1 5"/>
-          </svg>
-        </button>
-      </h2>
-      <div id="accordion-collapse-body-3" class="hidden" aria-labelledby="accordion-collapse-heading-3">
-        <div class="p-5 border border-t-0 border-gray-200">
-          <p class="mb-2 text-gray-500 ">The main difference is that the core components from Flowbite are open source under the MIT license, whereas Tailwind UI is a paid product. Another difference is that Flowbite relies on smaller and standalone components, whereas Tailwind UI offers sections of pages.</p>
-          <p class="mb-2 text-gray-500 ">However, we actually recommend using both Flowbite, Flowbite Pro, and even Tailwind UI as there is no technical reason stopping you from using the best of two worlds.</p>
-          <p class="mb-2 text-gray-500 ">Learn more about these technologies:</p>
-          <ul class="ps-5 text-gray-500 list-disc ">
-            <li><a href="https://flowbite.com/pro/" class="text-blue-600">Flowbite Pro</a></li>
-            <li><a href="https://tailwindui.com/" rel="nofollow" class="text-blue-600 ">Tailwind UI</a></li>
-          </ul>
-        </div>
-      </div>
-    </div>
+    <ul class="mt-5 mb-5 divide-y rounded-xl">
+      <li>
+        <details class="group">
+          <summary class="flex items-center gap-3 px-4 py-3 font-medium marker:content-none hover:cursor-pointer">
+            <svg class="w-5 h-5 text-gray-500 transition group-open:rotate-90" xmlns="http://www.w3.org/2000/svg"
+                 width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+              <path fill-rule="evenodd"
+                    d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z">
+              </path>
+            </svg>
+            English Books
+          </summary>
+          <ToDoListElem/>
+        </details>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
 import ToDoListMain from '@/components/ToDoListMain.vue'
+import ToDoListElem from '@/components/ToDoListElem.vue'
 
 export default {
   components: {
-    ToDoListMain
+    ToDoListMain,
+    ToDoListElem
   }
 }
 </script>
