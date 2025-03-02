9ce619920202c54d3d1752e7fb4e05109f95fc25
Braizom <a.a.r.boisseaux@student.vu.nl>
2025-03-02 01:13:35 -0500

    Added main section of todo lists page

---
+++ src/components/ToDoListMain.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="m-5">
+    <h1 class="text-3xl font-semibold">Books</h1>
+    <p class="m-4">You have 3 "books" to-do lists.</p>
+    <div class="w-full md:block md:w-auto">
+      <ul class="font-medium flex flex-col p-4 md:p-0 mt-4 border rounded-lg md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0">
+        <li>
+          <button type="button" @click="toggleListCreation" class="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 text-center inline-flex items-center">
+            <svg class="w-3.5 h-3.5 me-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 16 16">
+              <path d="M8 0a1 1 0 0 1 1 1v6h6a1 1 0 1 1 0 2H9v6a1 1 0 1 1-2 0V9H1a1 1 0 0 1 0-2h6V1a1 1 0 0 1 1-1z"/>
+            </svg>
+            Create a ToDo List
+          </button>
+        </li>
+        <li>
+          <button type="button" class="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 text-center inline-flex items-center">
+            Delete a ToDo List
+            <svg class="w-5 h-5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
+              <path d="M17.7427 8.46448L19.1569 9.87869L17.0356 12L19.157 14.1214L17.7428 15.5356L15.6214 13.4142L13.5 15.5355L12.0858 14.1213L14.2072 12L12.0859 9.87878L13.5002 8.46457L15.6214 10.5858L17.7427 8.46448Z" fill="#ffffff"></path> <path fill-rule="evenodd" clip-rule="evenodd" d="M8.58579 19L2.29289 12.7071C1.90237 12.3166 1.90237 11.6834 2.29289 11.2929L8.58579 5H22.5857V19H8.58579ZM9.41421 7L4.41421 12L9.41421 17H20.5857V7H9.41421Z" fill="#ffffff"></path>
+            </svg>
+          </button>
+        </li>
+      </ul>
+    </div>
+  </div>
+  <CreateList v-if="listCreation" :toggleListCreation="toggleListCreation"/>
+</template>
+
+<script>
+import CreateList from '@/components/CreateList.vue'
+
+export default {
+  components: {
+    CreateList
+  },
+  data () {
+    return {
+      listCreation: false
+    }
+  },
+  methods: {
+    toggleListCreation () {
+      this.listCreation = !this.listCreation
+    }
+  }
+}
+</script>
