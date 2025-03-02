1c002f7e2d6da96238671d702615828e0df5b992
Braizom <a.a.r.boisseaux@student.vu.nl>
2025-03-02 15:46:42 -0500

    code clean

--- src/components/RegisterForm.vue
+++ src/components/RegisterForm.vue
@@ -2,9 +2,8 @@
   <div class="m-10">
   <section class="bg-white">
     <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-      <a href="#" class="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white"></a>
       <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
-        <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
+        <div class="p-6 sm:p-8">
           <h1 class="text-xl font-bold leading-tight text-center tracking-tight text-gray-900 md:text-2xl dark:text-white">
             Create an account
           </h1>
@@ -81,7 +80,3 @@ export default {
   }
 }
 </script>
-
-<style scoped>
-
-</style>
