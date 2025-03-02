53390f1821a823181ea59a7fa253eee4aa0ad38d
Ibrahim Khan <ibrahim932@hotmail.fr>
2025-03-02 16:36:28 -0500

    formulaireProfile

---
+++ src/components/InformationPofil.vue
@@ -0,0 +1,21 @@
+<template>
+    <form class="max-w-sm mx-auto border border-gray-300 rounded-lg p-6 shadow-md">
+        <div class="mb-5">
+            <label for="firstname" class="block mb-2 text-sm font-medium text-gray-900 dark:text-black">Firstname</label>
+            <input type="text" id="firstname" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 white:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Nanthushan">
+        </div>
+        <div class="mb-5">
+            <label for="lastname" class="block mb-2 text-sm font-medium text-gray-900 dark:text-black">Lastname</label>
+            <input type="text" id="lastname" class="block w-full p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 text-xs focus:ring-blue-500 focus:border-blue-500 white:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Kesavan">
+        </div>
+        <div class="mb-5">
+            <label for="dob" class="block mb-2 text-sm font-medium text-gray-900 dark:text-black">Date of Birth</label>
+            <input type="date" id="dob" class="block w-full p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 text-xs focus:ring-blue-500 focus:border-blue-500 white:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500" value="2003-06-08">
+        </div>
+        <div class="mb-5">
+            <label for="bio" class="block mb-2 text-sm font-medium text-gray-900 dark:text-black">Bio</label>
+            <input type="text" id="bio" class="block w-full p-4 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 text-base focus:ring-blue-500 focus:border-blue-500 white:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Organized. Focused. Productive.">
+        </div>
+        <button type="submit" class="w-full bg-black text-white py-2 rounded-lg hover:bg-gray-800">Save</button>
+    </form>
+</template>
--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -1,5 +1,15 @@
 <template>
-  <div>
-    PROFILE
+  <div class="my-10">
+    <InformationPofil/>
   </div>
 </template>
+
+<script>
+import InformationPofil from '@/components/InformationPofil.vue'
+
+export default {
+  components: {
+    InformationPofil
+  }
+}
+</script>
