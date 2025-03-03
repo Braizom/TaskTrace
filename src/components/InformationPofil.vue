7d8124649022efb55454b300727fce16979050ac
Ibrahim Khan <ibrahim932@hotmail.fr>
2025-03-02 23:52:37 -0500

    AccountInformationdetailed

--- src/components/InformationPofil.vue
+++ src/components/InformationPofil.vue
@@ -1,5 +1,5 @@
 <template>
-    <form class="max-w-sm mx-auto border border-gray-300 rounded-lg p-6 shadow-md">
+    <form class="max-w-sm mx-auto border border-gray-300 rounded-lg p-6 shadow-md my-10">
         <div class="mb-5">
             <label for="firstname" class="block mb-2 text-sm font-medium text-gray-900 dark:text-black">Firstname</label>
             <input type="text" id="firstname" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 white:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Nanthushan">
--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -1,15 +1,19 @@
 <template>
   <div class="my-10">
     <InformationPofil/>
+    <AccountInformation/>
   </div>
+
 </template>
 
 <script>
 import InformationPofil from '@/components/InformationPofil.vue'
+import AccountInformation from '@/components/AccountInformationProfile.vue'
 
 export default {
   components: {
-    InformationPofil
+    InformationPofil,
+    AccountInformation
   }
 }
 </script>
