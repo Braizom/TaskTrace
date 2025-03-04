da429d84f5a0df0bc9b6999b758b8046b1f4073c
Braizom <a.a.r.boisseaux@student.vu.nl>
2025-03-03 22:11:09 -0500

    Linked JSON data throughout the ToDoList page

--- src/components/ToDoListElem.vue
+++ src/components/ToDoListElem.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="w-full md:block md:w-auto mx-20">
-    <ToDoListElemMenu/>
+    <ToDoListElemMenu :listName="list.name"/>
     <ul>
-      <li>
+      <li v-for="(elem, i) in list.elems" :key="i">
         <div class="p-6 bg-white border border-gray-200 rounded-lg shadow-sm my-2">
           <div class="w-full flex items-center">
             <svg class="h-52 w-52" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -10,7 +10,7 @@
             </svg>
             <div class="w-full ms-10">
               <div class="w-full flex flex-wrap justify-between items-center mb-2">
-                <h3 class="text-lg font-medium text-gray-900">Oliver Twist, Charles Dickens</h3>
+                <h3 class="text-lg font-medium text-gray-900">{{ elem.name }}</h3>
                 <button type="button" class="rtl:space-x-reverse text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 text-center inline-flex items-center">
                   <svg class="w-5 h-5 me-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
                     <path fill-rule="evenodd" clip-rule="evenodd" d="M4 12C4 11.4477 4.44772 11 5 11H19C19.5523 11 20 11.4477 20 12C20 12.5523 19.5523 13 19 13H5C4.44772 13 4 12.5523 4 12Z" fill="#ffffff"></path>
@@ -18,26 +18,7 @@
                   Delete Element
                 </button>
               </div>
-              <ul class="grid w-full gap-6 md:grid-cols-3">
-                <li>
-                  <input type="radio" id="1" name="hosting" value="1" class="hidden peer" checked>
-                  <label for="1" class="text-lg font-semibold inline-flex items-center justify-between w-full p-5 text-gray-500 bg-white border border-gray-200 rounded-lg cursor-pointer  peer-checked:bg-green-600  peer-checked:text-white hover:text-gray-600 hover:bg-gray-100 ">
-                    To Do
-                  </label>
-                </li>
-                <li>
-                  <input type="radio" id="2" name="hosting" value="2" class="hidden peer">
-                  <label for="2" class="text-lg font-semibold inline-flex items-center justify-between w-full p-5 text-gray-500 bg-white border border-gray-200 rounded-lg cursor-pointer  peer-checked:bg-green-600  peer-checked:text-white hover:text-gray-600 hover:bg-gray-100 ">
-                    Doing
-                  </label>
-                </li>
-                <li>
-                  <input type="radio" id="3" name="hosting" value="3" class="hidden peer">
-                  <label for="3" class="text-lg font-semibold inline-flex items-center justify-between w-full p-5 text-gray-500 bg-white border border-gray-200 rounded-lg cursor-pointer  peer-checked:bg-green-600  peer-checked:text-white hover:text-gray-600 hover:bg-gray-100 ">
-                    Done
-                  </label>
-                </li>
-              </ul>
+              <ToDoListElemStatus :status="elem.status" :elem="i" :list="listId"/>
             </div>
           </div>
         </div>
@@ -48,10 +29,18 @@
 
 <script>
 import ToDoListElemMenu from '@/components/ToDoListElemMenu.vue'
+import ToDoListElemStatus from '@/components/ToDoListElemStatus.vue'
 
 export default {
   components: {
-    ToDoListElemMenu
+    ToDoListElemMenu,
+    ToDoListElemStatus
+  },
+  props: ['list', 'listId'],
+  data (list) {
+    return {
+      elems: list.elems
+    }
   }
 }
 </script>
